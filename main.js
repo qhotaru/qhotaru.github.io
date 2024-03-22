2b5fe96604a4f559635573d0f07315fa410d446e
shino <q.hotaru@gmail.com>
2024-03-22 20:24:02 +0900

    use js

---
+++ main.js
@@ -0,0 +1,11 @@
+//
+// main.js
+//
+//
+// 2024/03/22 Rev.01 shino Initial Revision
+//
+// Copyright (C) K.Shinomiya, all rights reserved.
+//
+// console.log("I am a JavaScipt.");
+document.write("<h1> I am a Javascript.")
+
